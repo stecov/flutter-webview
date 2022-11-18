bbe15172bc4b4490448ca021590f11eee56b0277
scova <stefan.cova@razorfish.fr>
2022-11-18 11:00:20 +0100

    First build for Android 2(1.0.0)

---
+++ ios/webview_in_flutter/webview_in_flutter/webview_in_flutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_in_flutterApp.swift
+//  webview_in_flutter
+//
+//  Created by scova on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct webview_in_flutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
